e1602653242d0b4ce7e48b3dcff5c24a1d83ba26
overmet15 <metsocool675@gmail.com>
2025-05-21 21:35:16 +0900

    Enabled encryption

--- TNet/Server/LobbyUtils.cs
+++ TNet/Server/LobbyUtils.cs
@@ -61,20 +61,34 @@ internal class LobbyUtils
         data[7] = (byte)(num2 & 0xFFu & 0xFFu);
     }
 
-#pragma warning disable
     public static void Encrypt(Packet packet)
     {
         if (Lobby.blowFish == null) return;
 
-        /*ulong val = 0;
+        byte[] data = new byte[8];
 
         packet.Position = 0;
-        packet.PopUInt64(ref val);
-        fish.Encrypt(ref val);
+        packet.PopByteArray(ref data, 8);
+
+        uint num = (uint)((data[0] << 24) | (data[1] << 16) | (data[2] << 8) | data[3]);
+        uint num2 = (uint)((data[4] << 24) | (data[5] << 16) | (data[6] << 8) | data[7]);
+        ulong num3 = num;
+        num3 = (num3 << 32) + num2;
+        Lobby.blowFish.Encrypt(ref num3);
+        num = (uint)(num3 >> 32);
+        num2 = (uint)num3;
+        data[0] = (byte)(((num & 0xFF000000u) >> 24) & 0xFFu);
+        data[1] = (byte)(((num & 0xFF0000) >> 16) & 0xFFu);
+        data[2] = (byte)(((num & 0xFF00) >> 8) & 0xFFu);
+        data[3] = (byte)(num & 0xFFu & 0xFFu);
+        data[4] = (byte)(((num2 & 0xFF000000u) >> 24) & 0xFFu);
+        data[5] = (byte)(((num2 & 0xFF0000) >> 16) & 0xFFu);
+        data[6] = (byte)(((num2 & 0xFF00) >> 8) & 0xFFu);
+        data[7] = (byte)(num2 & 0xFFu & 0xFFu);
+
         packet.Position = 0;
-        packet.PushUInt64(val);*/
+        packet.PushByteArray(data);
     }
-#pragma warning restore
 
     /*
             uint num = (uint)((data[0] << 24) | (data[1] << 16) | (data[2] << 8) | data[3]);
